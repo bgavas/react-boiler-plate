abd01003f1744d61c7cb55c6dc724151a086e63c
Burak Gavas <burak.gavas@misonesolution.com>
2018-05-18 09:05:14 +0000

    Typo and changes

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import 'normalize.css/normalize.css';
+import './style/style.scss';
 
 ReactDOM.render(<p>Boiler plate</p>, document.getElementById('app'));
